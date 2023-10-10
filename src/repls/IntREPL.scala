c692da9b1a58ff301396ba69794120ca21853765
CarThrottlefan <alexandervici@gmail.com>
2023-10-10 13:06:59 +0200

    Shunting Yard algorithm implementation in progress

--- src/repls/IntREPL.scala
+++ src/repls/IntREPL.scala
@@ -1,4 +1,8 @@
 package repls
+import scala.collection.mutable
+import scala.collection.mutable.Stack
+import scala.util.matching.Regex
+import scala.collection.mutable.Queue
 
 class IntREPL extends REPLBase {
     // Have a REPL of type Int
@@ -7,9 +11,49 @@ class IntREPL extends REPLBase {
 
     override def readEval(command: String): String = {
         val elements = command.split("\\s") // split string based on whitespace
+        shuntingYard(elements)
         // TODO: complete me!
         ""
         //a function that implements shunting yard algorithm, then create a tree that represents the formula given as input
     }
     // TODO: Implement any further functions that are specifically for an IntREPL
+    def shuntingYard(input: Array[String]): Unit =
+    {
+        val numberPattern: Regex = "[0-9]+".r // i am using this just to define if it is a num
+        val letterPattern: Regex = "[a-zA-Z]+[0-9]*".r // i am using this just to define if it is a var
+        var operatorStack = Stack[String]()
+        var outputQueue = Queue[String]()
+
+        for(i <- input.indices)
+        {
+            (input(i)) match
+            {
+                //left bracket is just push on stack
+
+                //right bracket - pop till i find left bracket
+
+                // case + or - => a case where something higher precedence is on stack, if not another case to just push
+
+                // case * => push to stack
+
+                case "(" =>
+                    operatorStack.push(input(i))
+
+                case ")" =>
+                    while(operatorStack.top != "(")
+                    {
+                        operatorStack.pop()
+                    }
+
+                case "*" =>
+                    operatorStack.push(input(i))
+
+                case _ if numberPattern.findFirstMatchIn(input(i)).isDefined => //if it's a number
+                    outputQueue.enqueue(input(i))
+
+                case _ if letterPattern.findFirstMatchIn(input(i)).isDefined => //if it's a var
+
+            }
+        }
+    }
 }
