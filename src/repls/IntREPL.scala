6337df32d6deb4bb7e234f1848d77f702119e85b
CarThrottlefan <alexandervici@gmail.com>
2023-10-10 12:20:57 +0200

    First commit

--- src/repls/IntREPL.scala
+++ src/repls/IntREPL.scala
@@ -9,7 +9,7 @@ class IntREPL extends REPLBase {
         val elements = command.split("\\s") // split string based on whitespace
         // TODO: complete me!
         ""
+        //a function that implements shunting yard algorithm, then create a tree that represents the formula given as input
     }
-
     // TODO: Implement any further functions that are specifically for an IntREPL
 }
