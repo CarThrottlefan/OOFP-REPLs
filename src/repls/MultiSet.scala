63e0f68c4300ab9ab20bdd013ff47f4e7f7a2ffc
CarThrottlefan <alexandervici@gmail.com>
2023-10-17 15:34:07 +0200

    Passes 56 tests, all multiset ones

--- src/repls/MultiSet.scala
+++ src/repls/MultiSet.scala
@@ -17,23 +17,70 @@ case class MultiSet[T] (multiplicity: Map[T, Int]) {
         Example:
         {a,b,b,c,c,c} * {b,c,c,c,c} = {b,c,c,c}
      */
-    def *(that: MultiSet[T]): MultiSet[T] = empty[T]
-
+    def *(that: MultiSet[T]): MultiSet[T] = {
+        var returnMap: Map[T, Int] = Map()
+        multiplicity.foreach
+        {
+            case(elements, valueMap1) =>
+            if(that.multiplicity.contains(elements))
+            {
+            val valueMap2 = that.multiplicity(elements)
+            val min : Int = valueMap1.min(valueMap2)
+            returnMap += (elements -> min)
+            }
+        }
+        val set = new MultiSet(returnMap)
+        set
+    }
     /* TODO
         Summation of two multisets:
         ∀x m_c(x) = m_a(x) + m_b(x)
         Example:
         {a,b,c,c} + {a,c,d} = {a,a,b,c,c,c,d}
      */
-    def +(that: MultiSet[T]): MultiSet[T] = empty[T]
 
+    def +(that: MultiSet[T]): MultiSet[T] = {
     /* TODO
         Subtraction of two multisets:
         ∀x m_c(x) = max(m_a(x) - m_b(x), 0)
         Example:
         {a,b,b,d} - {b,c,c,d,d} = {a,b}
      */
-    def -(that: MultiSet[T]): MultiSet[T] = empty[T]
+    var returnMap: Map[T, Int] = Map()
+    multiplicity.foreach
+    {
+        case(elements, valueMap1) => returnMap += (elements -> valueMap1)
+    }
+    that.multiplicity.foreach
+    {
+        case(elements, valueMap2) =>
+            if(returnMap.contains(elements))
+                returnMap = returnMap.updated(elements, returnMap(elements) + valueMap2)
+            else
+                returnMap += (elements -> valueMap2)
+    }
+    val set = new MultiSet(returnMap)
+    set
+}
+    def -(that: MultiSet[T]): MultiSet[T] =
+    {
+        var returnMap: Map[T, Int] = Map()
+        multiplicity.foreach {
+            case (elements, valueMap1) =>
+                if(that.multiplicity.contains(elements))
+                {
+                    val valueMap2 = that.multiplicity(elements)
+                    val max = (valueMap1 - valueMap2).max(0)
+                    returnMap += (elements -> max)
+                }
+                else
+                {
+                    returnMap += (elements -> valueMap1)
+                }
+        }
+        val set = new MultiSet(returnMap)
+        set
+    }
     /* TODO
         Make sure a multiset can be returned as a sequence.
 
@@ -45,10 +92,10 @@ case class MultiSet[T] (multiplicity: Map[T, Int]) {
         var retSeq: Seq[T] = Seq()
         multiplicity.foreach{
             case (element, count) =>
-            for(i <- 1 to count)
-            {
-                retSeq = retSeq :+ element
-            }
+                for(i <- 1 to count)
+                {
+                    retSeq = retSeq :+ element
+                }
         }
         retSeq
     }
