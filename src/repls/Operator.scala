0062c848c083cdbe94687bc3d3d0cf6792c2963d
CarThrottlefan <alexandervici@gmail.com>
2023-10-17 18:22:44 +0200

    Passes 68 tests

--- src/repls/Operator.scala
+++ src/repls/Operator.scala
@@ -9,11 +9,22 @@ case class Operator(lhs: Expression, operatorName: String, rhs: Expression) exte
     PatternMatch.operatorByName(l, operatorName, r).toInt
   }
 
-  private def operatorByName(opName: String, lhs: Int, rhs: Int) = {
+  private def operatorByName(opName: String, lhs: Int, rhs: Int) =
+  {
     if (opName == "+") lhs + rhs
     else if (opName == "*") lhs * rhs
     else lhs - rhs
   }
 
-  override def toString: String = lhs.toString + " " + operatorName + " " + rhs.toString
+  override def toString: String =
+     this match {
+       case Operator(Operator(a, op1, b), operatorName, Operator(c, op2, d)) =>
+          "( " + a.toString + " " + op1 + " " + b.toString + " ) " + operatorName + " ( " + c.toString + " " + op2 + " " + d.toString + " )"
+       case Operator(Operator(a, op, b), operatorName, rhs) =>
+         "( " + a.toString + " " + op + " " + b.toString + " ) " + operatorName + " " + rhs.toString
+       case Operator(lhs, operatorName, Operator(c, op, d)) =>
+         lhs.toString + " " + operatorName + " ( " + c.toString + " " + op + " " + d.toString + " )"
+       case Operator(lhs, operatorName, rhs) =>
+         lhs.toString  + " " + operatorName + " "  + rhs.toString
+    }
 }
